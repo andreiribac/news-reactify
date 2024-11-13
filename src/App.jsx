f3111d0cc2e0e12f922755c20e9a9f0ce65cfeb3
andreiribac <ribacandrei@gmail.com>
2024-11-13 10:52:05 +0200

    test ios sistem

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
 import Header from "./components/Header/Header";
 import Main from "./pages/Main";
 
+export const iosDevices = [
+  "iPad Simulator",
+  "iPhone Simulator",
+  "iPod Simulator",
+  "iPad",
+  "iPhone",
+  "iPod",
+];
+
+export const deviceDetectionHook = () => {
+  // @ts-ignore
+  const { platform } = navigator.userAgentData || navigator;
+
+  return {
+    devicePlatform: platform,
+    isIos: iosDevices.includes(platform),
+  };
+};
+const { devicePlatform, isIos } = deviceDetectionHook();
+
 function App() {
-	return (
-		<>
-			<Header />
+
+   const containerStyle = {
+     height: isIos ? "90vh" : "100vh",
+     display: "flex",
+     alignItems: "center",
+     justifyContent: "center",
+     flexDirection: "column",
+     textAlign: "center",
+     backgroundColor: isIos ? "yellow" : "green",
+   };
+
+  return (
+    <>
+      <div style={containerStyle}>
+        <p>Платформа устройства: {devicePlatform}</p>
+        {isIos ? <p>Это устройство iOS.</p> : <p>Это не устройство iOS.</p>}
+      </div>
+      {/* <Header />
       <main className="main"> 
         <div className="wrapper">
           <Main />
         </div>
-      </main>
-		</>
-	);
+      </main> */}
+    </>
+  );
 }
 
 export default App;
